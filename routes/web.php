6d9fd3ad50aa20991d848fd2b06d3d2efd23cf6c
William Camargo <44587916+hotwyl@users.noreply.github.com>
2024-09-25 20:56:11 -0300

    rotas

--- routes/web.php
+++ routes/web.php
@@ -17,9 +17,7 @@ use App\Http\Controllers\UnidadeController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [HomeController::class, 'dashboard']);
 
 //Auth::routes();
 
